9fa523745b6acac59018961f8bcab811f65b7f1b
KhangVu <Khangv6002@gmail.com>
2025-12-22 16:08:04 +0700

    Tối ưu bằng duyệt từ dưới lên. Chú ý cơ chế tuần tự khác tuple unpacking của python

--- J01006.java
+++ J01006.java
@@ -1,18 +1,25 @@
 import java.util.Scanner;
-public class J01006{
-    public static int fibo(int i){ //đi từ trên xuống dễ TLE
-        if (i <= 1){
-            return i;
-        }else {
-            return fibo(i-1) + fibo(i-2);
+
+public class J01006 {
+    public static long fibo(int i) {
+        if (i <= 1) return i;
+        long a = 0;
+        long b = 1;
+        for (int j = 2; j <= i; j++) {
+            long next = a + b;
+            a = b;
+            b = next;
         }
+        return b;
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        while (n > 0){
+        while (n > 0) {
             int i = sc.nextInt();
             System.out.println(fibo(i));
+            n--;
         }
         sc.close();
     }
