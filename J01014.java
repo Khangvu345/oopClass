6c5e66f56a7e2105c3034a1e44a95848ec319037
KhangVu <Khangv6002@gmail.com>
2025-12-22 22:08:16 +0700

    fix bằng 1 vòng điều kiện nữa:

--- J01014.java
+++ J01014.java
@@ -13,11 +13,13 @@ public class J01014 {
 
         for (int i = 3; i * i <= n; i += 2) {
             int count = 0;
-            while (n % i == 0) {
+            if (n % i == 0) {
+                while (n % i == 0) {
                 count++;
                 n /= i;
+                }
+                System.out.print(i+"("+count+") ");
             }
-            System.out.print(i+"("+count+") "); // in ra bị dính cả trường hợp nếu thừa số đó không xuất hiện
         }
         if (n > 1) System.out.print(n+"(1)");
         System.out.println();
