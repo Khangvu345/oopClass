7fabd0b7b36a8afdb2aad78dcd8d8b5f1751e1c0
KhangVu <Khangv6002@gmail.com>
2025-12-22 15:56:40 +0700

    đệ quy fibo TLE nếu i lớn

---
+++ J01006.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class J01006{
+    public static int fibo(int i){
+        if (i <= 1){
+            return i;
+        }else {
+            return fibo(i-1) + fibo(i-2);
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while (n > 0){
+            int i = sc.nextInt();
+            System.out.println(fibo(i));
+        }
+        sc.close();
+    }
+}
