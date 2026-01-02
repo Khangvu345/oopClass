22c8d391d871ea473eccef131bb2589e1edf4263
KhangVu <Khangv6002@gmail.com>
2026-01-02 21:55:23 +0700

    bổ sung comment cho sx chèn

--- J02012.java
+++ J02012.java
@@ -5,10 +5,10 @@ public class J02012 {
             int key = a[i];
             int j = i - 1;
             while (j >=0 && a[j] > key) {
-                a[j+1] = a[j];
-                j--;
+                a[j+1] = a[j]; // Để trống chỗ để cài đặt giá trị nhỏ hơn
+                j--; // Di chuyển con trỏ j về trái 
             }
-            a[j + 1] = key;
+            a[j + 1] = key; // Cài đặt giá trị nhỏ hơn vào vị trí đúng sau khi con trỏ j đã về được vị trí gần đúng nhất, thêm 1 để đạt được vị trí cần đặt key
             System.out.print("Buoc " + (i) + ": ");
             for (int k = 0; k <= i; k++) {
                 System.out.print(a[k] + " ");
