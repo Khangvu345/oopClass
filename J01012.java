2ca4c9203e01d3c6176a414804de8e55657f4738
KhangVu <Khangv6002@gmail.com>
2026-01-03 12:26:14 +0700

    Uoc so chia het cho 2

--- J01012.java
+++ J01012.java
@@ -1,1 +1,23 @@
-// tutu
+import java.util.Scanner;
+public class J01012 {
+    public static int soUocSoChiaHetCho2(int n){
+        if (n % 2 != 0) return 0;
+        int count = 0;
+        for (int i = 1; i <= Math.sqrt(n); i++) // duyệt từ 1 đến căn bậc 2 của n{
+            if (n % i == 0) {
+                if (i % 2 == 0) count++;
+                int temp = n/i;
+                if ( temp != i && temp % 2 == 0) count++; // điều kiện để trừ trường hợp lặp của số chính phương
+            }
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            System.out.println(soUocSoChiaHetCho2(n));
+        }
+        sc.close();
+    }
+}
