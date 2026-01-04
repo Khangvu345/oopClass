09a4f46c77541c1b11e41db58c739303498ad139
KhangVu <Khangv6002@gmail.com>
2026-01-04 23:08:39 +0700

    Dien Thoai Cuc Gach: Cách dùng stringbuilder

--- J03021_DthCucGach.java
+++ J03021_DthCucGach.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 public class J03021_DthCucGach {
     public static String transferByLogic(String str){
-        String res = "";
+        StringBuilder res = new StringBuilder();
         str = str.toUpperCase();
         for (char c: str.toCharArray()){ // Nhớ chỉ có thể lặp qua 1 mảng chứ không phải String
             switch (c){
-                case 'A': case 'B': case 'C': res += "2"; break;
-                case 'D': case 'E': case 'F': res += "3"; break;
-                case 'G': case 'H': case 'I': res += "4"; break;
-                case 'J': case 'K': case 'L': res += "5"; break;
-                case 'M': case 'N': case 'O': res += "6"; break;
-                case 'P': case 'Q': case 'R': case 'S': res += "7"; break;
-                case 'T': case 'U': case 'V': res += "8"; break;
-                case 'W': case 'X': case 'Y': case 'Z': res += "9"; break;
+                case 'A': case 'B': case 'C': res.append("2"); break;
+                case 'D': case 'E': case 'F': res.append("3"); break;
+                case 'G': case 'H': case 'I': res.append("4"); break;
+                case 'J': case 'K': case 'L': res.append("5"); break;
+                case 'M': case 'N': case 'O': res.append("6"); break;
+                case 'P': case 'Q': case 'R': case 'S': res.append("7"); break;
+                case 'T': case 'U': case 'V': res.append("8"); break;
+                case 'W': case 'X': case 'Y': case 'Z': res.append("9"); break;
                 default: break;
             }
         }
-        return res;
+        return res.toString();
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
