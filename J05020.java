5abf294e30584773da6cee1c6e8c56dcf66a7f2a
KhangVu <Khangv6002@gmail.com>
2026-01-08 21:54:38 +0700

    Lỗi sai khi so sánh với String - chuỗi

---
+++ J05020.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class J05020 {
+    public static class SV{
+        private String maSV, hoTen, lop, email;
+        public SV(String maSV, String hoTen, String lop, String email){
+            this.maSV = maSV;
+            this.hoTen = hoTen;
+            this.lop = lop;
+            this.email = email;
+        }   
+        @Override
+        public String toString(){
+            return String.format("%s %s %s %s", maSV, hoTen, lop, email);
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        ArrayList<SV> ds = new ArrayList<>();
+        while(t-- > 0){
+            String maSV = sc.nextLine();
+            String hoTen = sc.nextLine();
+            String lop = sc.nextLine();
+            String email = sc.nextLine();
+            SV sv = new SV(maSV, hoTen, lop, email);
+            ds.add(sv);
+        }
+        // Lỗi sai kinh điển khi so sánh String không được so sánh != và == 
+        Collections.sort(ds, (a,b) -> {
+            if (a.lop != b.lop) return a.lop.compareTo(b.lop);
+            return a.maSV.compareTo(b.maSV);
+        });
+        for (SV sv: ds){
+            System.out.println(sv);
+        } 
+    }
+}
